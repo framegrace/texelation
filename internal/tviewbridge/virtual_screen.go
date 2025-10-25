c698d9ce16853e0234326ec5177481c875cf7cb3
Marc Gràcia <Marc Gracia>
2025-10-25 19:33:30 +0200

    Revert "Increase event and refresh channel buffers to prevent dropped notifications"
    This reverts commit 6ea89264f0029234ed5a7cea88c480602dfe9016.

--- internal/tviewbridge/virtual_screen.go
+++ internal/tviewbridge/virtual_screen.go
@@ -53,7 +53,7 @@ func NewVirtualScreen(width, height int) *VirtualScreen {
 		width:     width,
 		height:    height,
 		style:     tcell.StyleDefault,
-		eventChan: make(chan tcell.Event, 256), // Increased from 32 to handle rapid typing
+		eventChan: make(chan tcell.Event, 32),
 		stopChan:  make(chan struct{}),
 	}
 	vs.allocateBuffers()
@@ -233,7 +233,6 @@ func (vs *VirtualScreen) PostEvent(ev tcell.Event) error {
 		return nil
 	default:
 		// Drop event if channel is full
-		println("VirtualScreen.PostEvent: WARNING - Event channel full, dropping event!")
 		return nil
 	}
 }
@@ -277,7 +276,6 @@ func (vs *VirtualScreen) Show() {
 			select {
 			case vs.refreshChan <- true:
 			default: // Don't block if channel is full
-				println("VirtualScreen.Show: WARNING - Refresh channel full, dropping notification!")
 			}
 		}
 	}
@@ -433,7 +431,6 @@ func (vs *VirtualScreen) GetBuffer() [][]texel.Cell {
 		buffer[y] = make([]texel.Cell, width)
 		copy(buffer[y], vs.frontBuffer[y])
 	}
-
 	return buffer
 }
 
